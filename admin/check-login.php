ada4e87e63c03b4c8699a0d786122f18d01a6be3
amarjeet <amarjeet@xenett.com>
2025-04-14 12:25:32 +0530

    some change

--- admin/check-login.php
+++ admin/check-login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(isset($_SESSION['id']) && $_SESSION['login_type']=='user'){
+if(isset($_SESSION['id']) && $_SESSION['login_type']=='admin'){
 
 }
 else{
--- userdash/php-includes/connect.php
+++ userdash/php-includes/connect.php
@@ -8,7 +8,9 @@
 	if(mysqli_connect_error()){
 		echo 'connect to database failed';
 	}*/
-	error_reporting(E_ALL ^ E_NOTICE);
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+	
 ?>
 <?php
 $servername = "localhost";
